f4fc52a1f85ab07363b46dbcb57c6239ee27e57c
tsmalla <tjark.smalla@gmx.de>
2018-07-30 16:10:27 +0200

    add new element on enter

--- src/cljs/shackleton/views.cljs
+++ src/cljs/shackleton/views.cljs
@@ -19,22 +19,22 @@
 (defn matrix []
   (set! (.-onresize js/window) (fn [] (rf/dispatch [:set-window-dimension {:w js/innerWidth :h js/innerHeight}])))
   (rf/dispatch [:set-window-dimension {:w js/innerWidth :h js/innerHeight}])
-  (let [elements (rf/subscribe [:elements])
+  (let [elements         (rf/subscribe [:elements])
         window-dimension (rf/subscribe [:window-dimension])]
     (fn []
-      (let [max-fn (fn [key] (apply max (map (fn [el] (Math/abs (get el key))) @elements)))
-            x-max (max-fn :x)
-            y-max (max-fn :y)
-            z-max (apply max (map :z @elements))
-            z-min (apply min (map :z @elements))
-            w (:w @window-dimension)
-            h (* svg-height (:h @window-dimension))
-            hw (/ w 2)
-            hh (/ h 2)
-            x-max (* x-max 1.2)
-            y-max (* y-max 1.2)
-            x->svg (partial kart-x->svg w x-max)
-            y->svg (partial kart-y->svg h y-max)
+      (let [max-fn   (fn [key] (apply max (map (fn [el] (Math/abs (get el key))) @elements)))
+            x-max    (max-fn :x)
+            y-max    (max-fn :y)
+            z-max    (apply max (map :z @elements))
+            z-min    (apply min (map :z @elements))
+            w        (:w @window-dimension)
+            h        (* svg-height (:h @window-dimension))
+            hw       (/ w 2)
+            hh       (/ h 2)
+            x-max    (* x-max 1.2)
+            y-max    (* y-max 1.2)
+            x->svg   (partial kart-x->svg w x-max)
+            y->svg   (partial kart-y->svg h y-max)
             z->color (partial z->color-intensitiy z-min z-max)]
         [:section {:id "kartesian" :style {:position :absolute :top 0 :left 0 :right 0 :height (str (int (* 100 svg-height)) "%") :background-color "white"}}
          [:svg {:style {:width "100%" :height "100%"}}
@@ -45,14 +45,23 @@
 (defn bind-to-fn [atom] (fn [e] (reset! atom (.. e -target -value))))
 
 (defn add-element-dialog [active-dialog]
-  (let [title-i (r/atom "")
-        x-i (r/atom "")
-        y-i (r/atom "")
-        z-i (r/atom "")
-        link-i (r/atom "")
-        valid-i? (r/atom false)
-        validate-i-fn (fn [fn] (reset! valid-i? (not (or (empty? @x-i) (empty? @y-i) (empty? @z-i)))) fn)
-        input-changed-fn (comp validate-i-fn bind-to-fn)]
+  (let [title-i          (r/atom "")
+        x-i              (r/atom "")
+        y-i              (r/atom "")
+        z-i              (r/atom "")
+        link-i           (r/atom "")
+        valid-i?         (r/atom false)
+        validate-i-fn    (fn [fn] (reset! valid-i? (not (or (empty? @x-i) (empty? @y-i) (empty? @z-i)))) fn)
+        input-changed-fn (comp validate-i-fn bind-to-fn)
+        add-new-element  (fn []
+                           (when @valid-i?
+                             (rf/dispatch [:add-el @title-i
+                                           (cljsr/read-string @x-i)
+                                           (cljsr/read-string @y-i)
+                                           (cljsr/read-string @z-i)
+                                           @link-i])
+                             (reset! active-dialog nil)))
+        add-on-enter     (fn [e] (when (= (.-keyCode e) 13) (add-new-element)))]
     (fn [active-dialog]
       [:div.dialog.menu.add-element
        [:div.label-input-gr
@@ -66,23 +75,21 @@
         [:input {:value @y-i :on-input (input-changed-fn y-i)}]]
        [:div.label-input-gr
         [:text "z"]
-        [:input {:value @z-i :on-input (input-changed-fn z-i)}]]
+        [:input {:value       @z-i :on-input (input-changed-fn z-i)
+                 :on-key-down add-on-enter}]]
        [:div.label-input-gr
         [:text "Link (optional)"]
-        [:input {:value @link-i :on-input (input-changed-fn link-i)}]]
+        [:input {:value       @link-i :on-input (input-changed-fn link-i)
+                 :on-key-down add-on-enter}]]
        [:div.bottom-button-cont
-        [:button (merge {:on-click (fn [] (rf/dispatch [:add-el @title-i
-                                                        (cljsr/read-string @x-i)
-                                                        (cljsr/read-string @y-i)
-                                                        (cljsr/read-string @z-i)
-                                                        @link-i]) (reset! active-dialog nil))}
+        [:button (merge {:on-click add-new-element}
                         (if @valid-i? {} {:disabled "true"}))
          [font-awesome {:name "plus"}]]
         [:button {:on-click (fn [] (reset! active-dialog nil))}
          [font-awesome {:name "times"}]]]])))
 
 (defn share-dialog [active-dialog]
-  (let [db-hash (rf/subscribe [:db-as-hash])
+  (let [db-hash   (rf/subscribe [:db-as-hash])
         i-db-hash (r/atom "")]
     (fn [active-dialog]
       [:div.dialog.menu
@@ -102,8 +109,8 @@
 
 (defn main-panel []
   (let [active-dialog (r/atom nil)
-        toggle-fn (fn [key]
-                    (fn [] (if (= key @active-dialog) (reset! active-dialog nil) (reset! active-dialog key))))]
+        toggle-fn     (fn [key]
+                        (fn [] (if (= key @active-dialog) (reset! active-dialog nil) (reset! active-dialog key))))]
     (fn []
       [:div {:style {:display :flex :flex-direction :column}}
        [matrix]
